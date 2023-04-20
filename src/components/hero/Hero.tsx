2f8d43af39abaf2ed5e58ea3066461092969f5bf
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-20 23:11:58 +0800

    feat: add hero page

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,53 @@
+import {Box, Button, FormControl, TextField, Typography} from "@mui/material";
+import {useTheme} from "@mui/system";
+import {useState} from "react";
+import {useRouter} from "next/router";
+
+export default function Hero() {
+    const theme = useTheme();
+    const router = useRouter();
+    const [username, setUsername] = useState<string>("");
+
+    const onSubmit = () => {
+        // TODO: Add logic
+        router.push("/realm");
+    };
+    return <Box sx={{
+        minHeight: '100vh',
+        background: 'url("/sushi-med.jpg")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        marginLeft: {xs: '-600px', sm: '-300px', md: '0'},
+    }}>
+        <Box sx={{
+            minHeight: '100vh',
+            marginLeft: {xs: '600px', sm: '300px', md: '0'},
+            backdropFilter: 'brightness(40%)',
+            display: 'grid',
+            placeItems: 'center',
+            gridTemplateColumns: 'repeat(12,1fr)'
+        }}>
+            <FormControl sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 1.5,
+                gridColumn: {xs: '2/10 span', sm: '2/4 span', md: '1/4 span'}
+            }} >
+                <Typography variant="h3">Welcome!</Typography>
+                <Typography variant="body1">Choose a username to get started</Typography>
+                <TextField label="Username" InputLabelProps={{sx: {color: '#fff'}}} sx={{
+                    // Changes TextField label color
+                    "& .MuiFormLabel-root": {
+                        color: '#fff'
+                    },
+                    "& .MuiFormLabel-root.Mui-focused": {
+                        color: '#fff'
+                    }
+                }} value={username} onChange={(e) => {
+                    setUsername(e.target.value)
+                }}/>
+                <Button variant="contained" sx={{color: theme.palette.secondary.main}} onClick={onSubmit}>Enter</Button>
+            </FormControl>
+        </Box>
+    </Box>
+}
