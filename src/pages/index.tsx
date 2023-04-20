164ed8a650e80ac5903f79d4299de7f0ee810945
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-20 23:12:37 +0800

    feat: add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "@/components/Head";
-
+import Hero from "@/components/hero/Hero"
 export default function Home() {
     return (
         <>
             <Head/>
+            <Hero/>
         </>
     )
 }
