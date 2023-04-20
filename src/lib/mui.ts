80768965eb861ca1b2200e673dcba8c83512a174
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-20 23:01:56 +0800

    feat: add custom mui theme

---
+++ src/lib/mui.ts
@@ -0,0 +1,35 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#FC6D33",
+        },
+        secondary: {
+            main: "#FFDBCC",
+        },
+    },
+    components: {
+        MuiTypography: {
+            defaultProps: {color: "#fff"}
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    color: "#fff",
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff"
+                    },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff"
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff"
+                    },
+                },
+            }
+        },
+    }
+});
+
+export default theme;
