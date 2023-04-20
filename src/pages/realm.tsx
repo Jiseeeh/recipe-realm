ad870b272c75af9bfc52e86dc2ff766fa2b52a59
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-21 01:25:27 +0800

    feat: add recipes page

---
+++ src/pages/realm.tsx
@@ -0,0 +1,31 @@
+import {Typography} from "@mui/material";
+
+import Sidebar from "@/components/sidebar/Sidebar";
+
+export default function Realm() {
+
+    return (
+        <>
+            <Sidebar>
+                <Typography paragraph>
+                    Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Blandit massa enim nec dui
+                    nunc. At augue eget arcu dictum varius duis at consectetur lorem. Tincidunt lobortis feugiat vivamus
+                    at augue eget. Mauris cursus mattis molestie a iaculis at erat pellentesque. Nunc vel risus commodo
+                    viverra maecenas accumsan. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus.
+                    Mauris pellentesque pulvinar pellentesque habitant morbi tristique. Congue mauris rhoncus aenean vel
+                    elit scelerisque mauris pellentesque pulvinar. Ut placerat orci nulla pellentesque dignissim enim
+                    sit. Neque sodales ut etiam sit amet. Blandit libero volutpat sed cras ornare.
+                </Typography>
+                <Typography paragraph>
+                    Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Blandit massa enim nec dui
+                    nunc. At augue eget arcu dictum varius duis at consectetur lorem. Tincidunt lobortis feugiat vivamus
+                    at augue eget. Mauris cursus mattis molestie a iaculis at erat pellentesque. Nunc vel risus commodo
+                    viverra maecenas accumsan. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus.
+                    Mauris pellentesque pulvinar pellentesque habitant morbi tristique. Congue mauris rhoncus aenean vel
+                    elit scelerisque mauris pellentesque pulvinar. Ut placerat orci nulla pellentesque dignissim enim
+                    sit. Neque sodales ut etiam sit amet. Blandit libero volutpat sed cras ornare.
+                </Typography>
+            </Sidebar>
+        </>
+    );
+}
