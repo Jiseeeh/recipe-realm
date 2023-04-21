70a98e92d8b840d2c56b411755a50aa1601a1ea5
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-21 15:46:45 +0800

    feat: add placeholders

--- src/pages/realm.tsx
+++ src/pages/realm.tsx
@@ -1,30 +1,55 @@
-import {Typography} from "@mui/material";
+import {Grid} from "@mui/material"
 
 import Sidebar from "@/components/sidebar/Sidebar";
+import RecipeCard from "@/components/recipe/RecipeCard";
+
 
 export default function Realm() {
+    const data = [
+        {
+            id: 0,
+            title: 'Secret Recipe',
+            imageLink: '/sushi-med.jpg',
+            description: "To make sushi, you will need sushi rice, which is a short-grain rice that is sticky when cooked. Begin by rinsing the rice until the water runs clear, and then cook the rice in water using a rice cooker or on the stovetop.",
+        },
+        {
+            id: 1,
+            title: 'Sushi master',
+            imageLink: '/sushi-med.jpg',
+            description: "While the rice is cooking, prepare your fillings. Popular choices include raw fish, such as salmon or tuna, as well as avocado, cucumber, and carrots. You can also add cooked ingredients, such as shrimp or crab.",
+        },
+        {
+            id: 2,
+            title: 'Master sushi',
+            imageLink: '/sushi-med.jpg',
+            description: "Once the rice is cooked, mix in a combination of rice vinegar, sugar, and salt to create the seasoning. Spread the seasoned rice onto a flat surface, such as a sushi mat or a piece of plastic wrap.",
+        },
+        {
+            id: 3,
+            title: 'Pro sushi',
+            imageLink: '/sushi-med.jpg',
+            description: "Add your desired fillings on top of the rice, and then roll the sushi tightly using the mat or plastic wrap. Cut the sushi into bite-sized pieces and serve with soy sauce, wasabi, and pickled ginger.",
+        },
+        {
+            id: 4,
+            title: 'Japanese Ultra Pro Max Mega Delux Sushi',
+            imageLink: '/sushi-med.jpg',
+            description: "Sushi is a traditional Japanese dish made from seasoned sushi rice combined with a variety of fillings, such as raw fish, vegetables, and other ingredients. The dish is often served with soy sauce, wasabi, and pickled ginger.",
+        },
 
+    ]
     return (
         <>
             <Sidebar>
-                <Typography paragraph>
-                    Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Blandit massa enim nec dui
-                    nunc. At augue eget arcu dictum varius duis at consectetur lorem. Tincidunt lobortis feugiat vivamus
-                    at augue eget. Mauris cursus mattis molestie a iaculis at erat pellentesque. Nunc vel risus commodo
-                    viverra maecenas accumsan. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus.
-                    Mauris pellentesque pulvinar pellentesque habitant morbi tristique. Congue mauris rhoncus aenean vel
-                    elit scelerisque mauris pellentesque pulvinar. Ut placerat orci nulla pellentesque dignissim enim
-                    sit. Neque sodales ut etiam sit amet. Blandit libero volutpat sed cras ornare.
-                </Typography>
-                <Typography paragraph>
-                    Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Blandit massa enim nec dui
-                    nunc. At augue eget arcu dictum varius duis at consectetur lorem. Tincidunt lobortis feugiat vivamus
-                    at augue eget. Mauris cursus mattis molestie a iaculis at erat pellentesque. Nunc vel risus commodo
-                    viverra maecenas accumsan. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus.
-                    Mauris pellentesque pulvinar pellentesque habitant morbi tristique. Congue mauris rhoncus aenean vel
-                    elit scelerisque mauris pellentesque pulvinar. Ut placerat orci nulla pellentesque dignissim enim
-                    sit. Neque sodales ut etiam sit amet. Blandit libero volutpat sed cras ornare.
-                </Typography>
+                <Grid container spacing={{xs: 2, md: 3}} justifyContent="center">
+
+                    {data.map(recipe => (
+                        <Grid item key={recipe.id} xs={12} sm={6} md={4} lg={2}>
+                            <RecipeCard imageLink={recipe.imageLink}
+                                        title={recipe.title}
+                                        description={recipe.description}/>
+                        </Grid>))}
+                </Grid>
             </Sidebar>
         </>
     );
