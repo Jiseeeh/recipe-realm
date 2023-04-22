cb72d20c7591499cdbd2e9f99d32f5c6eeaab7be
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-22 19:30:56 +0800

    feat: add component for loader

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,15 @@
+export default function Loader() {
+    // can be reusable by props
+    return <div className="loader-container">
+        <div className="loader">
+            <p className="loading">preparing</p>
+            <div className="words">
+                <span className="word">ingredients</span>
+                <span className="word">recipes</span>
+                <span className="word">seasonings</span>
+                <span className="word">condiments</span>
+                <span className="word">specials</span>
+            </div>
+        </div>
+    </div>
+}
