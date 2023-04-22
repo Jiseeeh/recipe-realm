5adba8eaf0fe9f4781be450cca196b3fd0304636
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-22 19:27:35 +0800

    feat: add link to recipe
    Also make props an interface

--- src/components/recipe/RecipeCard.tsx
+++ src/components/recipe/RecipeCard.tsx
@@ -1,15 +1,17 @@
 import {Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography} from "@mui/material"
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import {useTheme} from "@mui/system";
+import {useRouter} from "next/router";
 
-interface RecipeCardProps {
-    imageLink: string;
-    title: string;
-    description: string;
-}
+import Recipe from "@/types/recipe";
 
-export default function RecipeCard({imageLink, title, description}: RecipeCardProps) {
+export default function RecipeCard({id, imageLink, title, description}: Recipe) {
     const theme = useTheme();
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/recipe/${id}`);
+    };
 
     return <Card sx={{maxWidth: 280}}>
         <CardActionArea>
@@ -33,7 +35,7 @@ export default function RecipeCard({imageLink, title, description}: RecipeCardPr
         </CardContent>
         <CardActions>
             <Button size="small" variant="contained" color="primary"
-                    sx={{color: theme.palette.secondary.main, marginLeft: "auto"}}>Learn
+                    sx={{color: theme.palette.secondary.main, marginLeft: "auto"}} onClick={onClick}>Learn
                 More <ChevronRightIcon/></Button>
         </CardActions>
     </Card>
