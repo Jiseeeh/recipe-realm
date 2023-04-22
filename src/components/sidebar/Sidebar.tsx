ea4b773caa111a2abc85b07227a48431055ef7a3
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-22 19:29:45 +0800

    chore: reformat code

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,24 +1,24 @@
 import {ReactNode, useState} from "react";
 import {useRouter} from "next/router";
-import {CSSObject, styled, Theme, useTheme} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Divider from '@mui/material/Divider';
-import MenuIcon from '@mui/icons-material/Menu';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import HomeIcon from '@mui/icons-material/Home';
-import BorderColorIcon from '@mui/icons-material/BorderColor';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import MuiDrawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import ListItemText from '@mui/material/ListItemText';
+import {CSSObject, styled, Theme, useTheme} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiAppBar, {AppBarProps as MuiAppBarProps} from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import HomeIcon from "@mui/icons-material/Home";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import MuiDrawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import ListItemText from "@mui/material/ListItemText";
 
 import Head from "@/components/Head";
 
@@ -27,29 +27,29 @@ const iconColor = "1d1d1f";
 
 const openedMixin = (theme: Theme): CSSObject => ({
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.enteringScreen,
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
         width: `calc(${theme.spacing(8)} + 1px)`,
     },
 });
 
-const DrawerHeader = styled('div')(({theme}) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({theme}) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
@@ -60,39 +60,39 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
+    shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({theme, open}) => ({
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
     }),
     ...(open && {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
+        transition: theme.transitions.create(["width", "margin"], {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.enteringScreen,
         }),
     }),
 }));
 
-const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
-    ({theme, open}) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
+const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== "open",
+})(({theme, open}) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
+    ...(open && {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
     }),
-);
+    ...(!open && {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
+    }),
+}));
 
 interface SidebarProps {
     children: ReactNode;
@@ -114,7 +114,7 @@ export default function Sidebar({children}: SidebarProps) {
     return (
         <>
             <Head/>
-            <Box sx={{display: 'flex'}}>
+            <Box sx={{display: "flex"}}>
                 <CssBaseline/>
                 <AppBar position="fixed" open={open}>
                     <Toolbar>
@@ -125,12 +125,18 @@ export default function Sidebar({children}: SidebarProps) {
                             edge="start"
                             sx={{
                                 marginRight: 5,
-                                ...(open && {display: 'none'}),
+                                ...(open && {display: "none"}),
                             }}
                         >
                             <MenuIcon sx={{color: `#${iconColor}`}}/>
                         </IconButton>
-                        <Typography variant="h5" noWrap component="div" fontWeight="bold" sx={{color: `#${iconColor}`}}>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="div"
+                            fontWeight="bold"
+                            sx={{color: `#${iconColor}`}}
+                        >
                             Recipe Realm
                         </Typography>
                     </Toolbar>
@@ -138,27 +144,38 @@ export default function Sidebar({children}: SidebarProps) {
                 <Drawer variant="permanent" open={open}>
                     <DrawerHeader>
                         <IconButton onClick={handleDrawerClose}>
-                            {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                            {theme.direction === "rtl" ? (
+                                <ChevronRightIcon/>
+                            ) : (
+                                <ChevronLeftIcon/>
+                            )}
                         </IconButton>
                     </DrawerHeader>
                     <Divider/>
                     <List>
-                        <ListItem disablePadding sx={{
-                            display: 'block',
-                            backgroundColor: `${router.pathname === "/realm" ? theme.palette.secondary.main : ""}`
-                        }}>
+                        <ListItem
+                            disablePadding
+                            sx={{
+                                display: "block",
+                                backgroundColor: `${
+                                    router.pathname === "/realm"
+                                        ? theme.palette.secondary.main
+                                        : ""
+                                }`,
+                            }}
+                        >
                             <ListItemButton
                                 sx={{
                                     minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
+                                    justifyContent: open ? "initial" : "center",
                                     px: 2.5,
                                 }}
                             >
                                 <ListItemIcon
                                     sx={{
                                         minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
+                                        mr: open ? 3 : "auto",
+                                        justifyContent: "center",
                                     }}
                                 >
                                     <HomeIcon sx={{color: `#${iconColor}`}}/>
@@ -166,22 +183,29 @@ export default function Sidebar({children}: SidebarProps) {
                                 <ListItemText primary="Home" sx={{opacity: open ? 1 : 0}}/>
                             </ListItemButton>
                         </ListItem>
-                        <ListItem disablePadding sx={{
-                            display: 'block',
-                            backgroundColor: `${router.pathname === "/recipe/create" ? theme.palette.secondary.main : ""}`
-                        }}>
+                        <ListItem
+                            disablePadding
+                            sx={{
+                                display: "block",
+                                backgroundColor: `${
+                                    router.pathname === "/recipe/create"
+                                        ? theme.palette.secondary.main
+                                        : ""
+                                }`,
+                            }}
+                        >
                             <ListItemButton
                                 sx={{
                                     minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
+                                    justifyContent: open ? "initial" : "center",
                                     px: 2.5,
                                 }}
                             >
                                 <ListItemIcon
                                     sx={{
                                         minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
+                                        mr: open ? 3 : "auto",
+                                        justifyContent: "center",
                                     }}
                                 >
                                     <BorderColorIcon sx={{color: `#${iconColor}`}}/>
@@ -192,7 +216,10 @@ export default function Sidebar({children}: SidebarProps) {
                     </List>
                     <Divider/>
                 </Drawer>
-                <Box component="main" sx={{flexGrow: 1, p: 3}}>
+                <Box
+                    component="main"
+                    sx={{flexGrow: 1, p: 3, display: "flex", flexDirection: "column"}}
+                >
                     <DrawerHeader/>
                     {children}
                 </Box>
