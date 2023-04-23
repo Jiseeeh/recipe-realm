333da7304c00cb44ff0a29e727009f28735d1ab2
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-24 00:07:09 +0800

    feat: add fetch from db

--- src/pages/realm.tsx
+++ src/pages/realm.tsx
@@ -1,56 +1,60 @@
-import {Grid} from "@mui/material"
+import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 import Sidebar from "@/components/sidebar/Sidebar";
+import IRecipe from "@/types/recipe";
 import RecipeCard from "@/components/recipe/RecipeCard";
-
+import Loader from "@/components/loader/Loader";
+import Head from "@/components/Head";
 
 export default function Realm() {
-    const data = [
-        {
-            id: 0,
-            title: 'Secret Recipe',
-            imageLink: '/sushi-med.jpg',
-            description: "To make sushi, you will need sushi rice, which is a short-grain rice that is sticky when cooked. Begin by rinsing the rice until the water runs clear, and then cook the rice in water using a rice cooker or on the stovetop.",
-        },
-        {
-            id: 1,
-            title: 'Sushi master',
-            imageLink: '/sushi-med.jpg',
-            description: "While the rice is cooking, prepare your fillings. Popular choices include raw fish, such as salmon or tuna, as well as avocado, cucumber, and carrots. You can also add cooked ingredients, such as shrimp or crab.",
-        },
-        {
-            id: 2,
-            title: 'Master sushi',
-            imageLink: '/sushi-med.jpg',
-            description: "Once the rice is cooked, mix in a combination of rice vinegar, sugar, and salt to create the seasoning. Spread the seasoned rice onto a flat surface, such as a sushi mat or a piece of plastic wrap.",
-        },
-        {
-            id: 3,
-            title: 'Pro sushi',
-            imageLink: '/sushi-med.jpg',
-            description: "Add your desired fillings on top of the rice, and then roll the sushi tightly using the mat or plastic wrap. Cut the sushi into bite-sized pieces and serve with soy sauce, wasabi, and pickled ginger.",
-        },
-        {
-            id: 4,
-            title: 'Japanese Ultra Pro Max Mega Delux Sushi',
-            imageLink: '/sushi-med.jpg',
-            description: "Sushi is a traditional Japanese dish made from seasoned sushi rice combined with a variety of fillings, such as raw fish, vegetables, and other ingredients. The dish is often served with soy sauce, wasabi, and pickled ginger.",
-        },
+  const [recipes, setRecipes] = useState<IRecipe[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    ]
+  useEffect(() => {
+    (async function () {
+      const res = await axios.get("http://localhost:3001/api/recipe");
+
+      if (res.status !== 200) {
+        toast.error("Error fetching, please try again later.");
+        return;
+      }
+
+      setRecipes(res.data);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  if (!isLoading) {
     return (
-        <>
-            <Sidebar>
-                <Grid container spacing={{xs: 2, md: 3}} justifyContent="center">
-
-                    {data.map(recipe => (
-                        <Grid item key={recipe.id} xs={12} sm={6} md={4} lg={2}>
-                            <RecipeCard id={String(recipe.id)} author="" ingredients="" imageLink={recipe.imageLink}
-                                        title={recipe.title}
-                                        description={recipe.description}/>
-                        </Grid>))}
+      <>
+        <Sidebar>
+          <Grid container spacing={{ xs: 2, md: 3 }} justifyContent="center">
+            {recipes.length > 0 &&
+              recipes.map((recipe) => (
+                <Grid item key={recipe.id} xs={12} sm={6} md={4} lg={2}>
+                  <RecipeCard
+                    id={recipe.id}
+                    author_name={recipe.author_name}
+                    ingredients={recipe.ingredients}
+                    image_link={recipe.image_link}
+                    title={recipe.title}
+                    description={recipe.description}
+                    private_id={recipe.private_id}
+                  />
                 </Grid>
-            </Sidebar>
-        </>
+              ))}
+          </Grid>
+        </Sidebar>
+      </>
     );
+  }
+  return (
+    <>
+      <Head />
+      <Loader />
+    </>
+  );
 }
