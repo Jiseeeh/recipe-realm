90bfced7f5b896737820c259be72966f288352a9
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-23 15:53:18 +0800

    refractor: extract color

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,6 +3,7 @@ import {useTheme} from "@mui/system";
 import {useState} from "react";
 import {useRouter} from "next/router";
 
+const inputColor = 'f5f5f7';
 export default function Hero() {
     const theme = useTheme();
     const router = useRouter();
@@ -32,17 +33,18 @@ export default function Hero() {
                 flexDirection: 'column',
                 gap: 1.5,
                 gridColumn: {xs: '2/10 span', sm: '2/4 span', md: '1/4 span'}
-            }} >
+            }}>
                 <Typography variant="h3" color="#f5f5f7">Welcome!</Typography>
                 <Typography variant="body1" color="#f5f5f7">Choose a username to get started</Typography>
-                <TextField label="Username" InputLabelProps={{sx: {color: '#fff'}}} sx={{
+                <TextField label="Username" InputLabelProps={{sx: {color: `#${inputColor}`}}} sx={{
                     // Changes TextField label color
                     "& .MuiFormLabel-root": {
-                        color: '#fff'
+                        color: `#${inputColor}`
                     },
                     "& .MuiFormLabel-root.Mui-focused": {
-                        color: '#fff'
-                    }
+                        color: `#${inputColor}`
+                    },
+                    input: {color: `#${inputColor}`}
                 }} value={username} onChange={(e) => {
                     setUsername(e.target.value)
                 }}/>
