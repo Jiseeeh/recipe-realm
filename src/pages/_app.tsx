e9a49f7ca91aa54a5e47eeb0769b6480717fa869
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-23 20:15:44 +0800

    feat: add react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from "@mui/system";
 import {CssBaseline} from "@mui/material";
+import {Toaster} from "react-hot-toast";
 
 import theme from "@/lib/mui";
 import "@/components/loader.css";
@@ -10,6 +11,7 @@ export default function App({Component, pageProps}: AppProps) {
         <ThemeProvider theme={theme}>
             <Component {...pageProps} />
             <CssBaseline/>
+            <Toaster/>
         </ThemeProvider>
     </>
 }
