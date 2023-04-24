a8b317f6bd779c44aa62c055214c8552b66e5f7e
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-24 19:18:17 +0800

    feat: replace placeholder with data from db

--- src/pages/recipe/[id].tsx
+++ src/pages/recipe/[id].tsx
@@ -1,9 +1,14 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Box, Button, Chip, Fade, Modal, TextField, Typography} from "@mui/material";
 import {useTheme} from "@mui/system";
+import {useRouter} from "next/router";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 import Sidebar from "@/components/sidebar/Sidebar";
 import Loader from "@/components/loader/Loader";
+import Head from "@/components/Head";
+import IRecipe from "@/types/recipe";
 
 const modalStyle = {
     position: 'absolute',
@@ -21,10 +26,12 @@ const modalStyle = {
 }
 
 export default function Recipe() {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [privateId, setPrivateId] = useState("");
+    const [recipe, setRecipe] = useState<IRecipe | null>(null);
     const theme = useTheme();
+    const router = useRouter();
 
     const handleModalOpen = () => {
         setIsModalOpen(true);
@@ -41,7 +48,22 @@ export default function Recipe() {
         handleModalOpen();
     };
 
-    if (!isLoading) {
+    useEffect(() => {
+        (async function() {
+            const {id} = router.query;
+            const res = await axios.get(`${process.env.API}/recipe/${id}`);
+
+            if (res.status !== 200) {
+                toast.error("Error fetching, please try again later.");
+                return;
+            }
+            
+            setRecipe(res.data);
+            setIsLoading(false);
+        })()
+    },[]);
+
+    if (!isLoading && recipe) {
         return (
             <Sidebar>
                 <Box sx={{display: "flex", flexDirection: "column", gap: 1}}>
@@ -53,13 +75,13 @@ export default function Recipe() {
                         }}
                     >
                         <Typography variant="h4" fontWeight="bold" textAlign="center">
-                            Recipe Name
+                            {recipe.name}
                         </Typography>
-                        <Typography variant="h5">by John Doe</Typography>
+                        <Typography variant="h5">by {recipe.author_name}</Typography>
                     </Box>
                     <Box
                         component="img"
-                        src="/sushi-med.jpg"
+                        src={recipe.image_link}
                         sx={{
                             maxHeight: 300,
                             width: "100%",
@@ -67,51 +89,10 @@ export default function Recipe() {
                         }}
                     />
                     <Box sx={{display: "flex", gap: 1.5, flexWrap: "wrap"}}>
-                        <Chip label="soy sauce" variant="filled" color="secondary"/>
-                        <Chip label="water" variant="filled" color="secondary"/>
-                        <Chip label="sample" variant="filled" color="secondary"/>
-                        <Chip label="placeholder" variant="filled" color="secondary"/>
-                        <Chip label="long placeholder" variant="filled" color="secondary"/>
+                        {recipe.ingredients.split("\n").map((ingredient,index) => (<Chip key={index} label={ingredient} color="secondary"/>))}
                     </Box>
                     <Typography paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                        scelerisque justo eu lorem venenatis, ut accumsan mauris mollis.
-                        Nunc mollis hendrerit purus, a elementum ligula gravida id. Donec
-                        nunc elit, interdum ac commodo id, euismod tempor ligula. Aenean
-                        eleifend scelerisque nunc vestibulum ullamcorper. Nulla eu sem eu
-                        ligula rhoncus vehicula. Duis ultricies egestas tellus sit amet
-                        consectetur. Nulla in fermentum sapien, vitae tincidunt erat. Etiam
-                        viverra vel lectus a bibendum. Quisque pharetra quis arcu
-                        pellentesque luctus. Curabitur sed tincidunt neque. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Tempore a accusamus at
-                        reiciendis! Ab maiores illo quia temporibus hic veniam eum iste
-                        velit cupiditate incidunt blanditiis, repellendus tempore explicabo
-                        nam quidem cum? Alias dicta reiciendis laboriosam? Incidunt libero
-                        omnis ad quis nihil explicabo cumque ratione molestiae ipsa, sint
-                        corporis rem temporibus officiis atque aperiam hic doloribus? Hic
-                        voluptas vitae alias consectetur impedit eaque ex quae commodi
-                        tempore, sed ad explicabo quibusdam eum saepe cupiditate tenetur
-                        non, quo natus adipisci perspiciatis optio! Repellendus voluptatibus
-                        inventore itaque quasi perferendis quam. Quas quia ea, culpa impedit
-                        voluptas voluptatum a, quis fugiat officia est voluptatem excepturi
-                        voluptatibus nam quos? Consectetur consequatur porro ipsam laborum
-                        ipsum animi eum in, consequuntur, et officia voluptatem minus!
-                        Numquam molestias recusandae iure est, natus quod optio eveniet
-                        architecto omnis sapiente commodi, rerum voluptas, molestiae
-                        perspiciatis magni iste qui odio tempore aperiam! Numquam quos
-                        architecto temporibus adipisci quasi itaque maiores vitae deleniti
-                        ducimus, ex nobis, velit accusantium alias ab unde voluptates
-                        possimus ipsam facilis quidem quibusdam, earum maxime tenetur
-                        sapiente? Debitis veniam deleniti voluptatum laboriosam libero odio
-                        itaque quis consequuntur illo, harum placeat eaque alias
-                        reprehenderit accusamus. Necessitatibus exercitationem illo magni
-                        est officiis voluptas atque possimus quam hic alias? Maxime, velit
-                        iste! Error, saepe soluta illo voluptas, nemo natus minus veritatis
-                        ullam laborum quisquam nulla, culpa explicabo reiciendis laudantium
-                        totam omnis dolores consequatur eaque. Fugiat ea fugit voluptas
-                        necessitatibus, aspernatur dolores beatae nulla excepturi commodi
-                        veritatis reiciendis reprehenderit, ducimus illum. Repudiandae esse
-                        quaerat ipsa voluptatum? At perferendis reiciendis illum assumenda.
+                        {recipe.description}
                     </Typography>
                     <Box sx={{marginLeft: "auto"}}>
                         <Button onClick={onDelete}>Delete</Button>
@@ -138,12 +119,14 @@ export default function Recipe() {
                                        }}/>
                             <Button variant="contained" sx={{color: `${theme.palette.secondary.main}`}}>Submit</Button>
                         </Box>
-
                     </Fade>
                 </Modal>
             </Sidebar>
         );
     }
 
-    return <Loader/>;
+    return (<>
+        <Head/>
+        <Loader/>
+    </>);
 }
