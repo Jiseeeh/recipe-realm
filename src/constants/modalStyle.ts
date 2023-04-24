11a6588d779a5bd9162813b183b49a4fe054a183
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-24 20:35:36 +0800

    feat: make constant styles for modal

---
+++ src/constants/modalStyle.ts
@@ -0,0 +1,16 @@
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  maxWidth: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  display: "flex",
+  flexDirection: "column",
+  gap: 1,
+  p: 4,
+};
+
+export default modalStyle;
