74fec7095da612cd16dadc51bfb374890db1c19f
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-24 20:34:44 +0800

    refractor: extract modal style into a const

--- src/pages/recipe/[id].tsx
+++ src/pages/recipe/[id].tsx
@@ -9,21 +9,7 @@ import Sidebar from "@/components/sidebar/Sidebar";
 import Loader from "@/components/loader/Loader";
 import Head from "@/components/Head";
 import IRecipe from "@/types/recipe";
-
-const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '90%',
-    maxWidth: 400,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 1,
-    p: 4,
-}
+import modalStyle from "@/constants/modalStyle";
 
 export default function Recipe() {
     const [isLoading, setIsLoading] = useState(true);
