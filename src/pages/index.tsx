15e8adbab3401485ee97c13e1b18ffa09b170834
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-26 15:09:57 +0800

    refractor: extract head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,4 @@
-import Head from "@/components/Head";
 import Hero from "@/components/hero/Hero";
 export default function Home() {
-  return (
-    <>
-      <Head />
-      <Hero authType="Login" />
-    </>
-  );
+  return <Hero authType="Login" />;
 }
