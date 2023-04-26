6b600d67710f065c253fc640c730b8b78f0ed24a
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-26 15:53:14 +0800

    feat: redirect to login if empty local

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,23 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/system";
 import { CssBaseline } from "@mui/material";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import theme from "@/lib/mui";
 import "@/components/loader.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+      router.push("/");
+    }
+  }, [router]);
+
   return (
     <>
       <ThemeProvider theme={theme}>
