f63c1b6d2506c3ddb6044b616318c7ef4e58bd5a
Jiseeeh <johncarlo.camara1@gmail.com>
2023-05-06 22:52:24 +0800

    feat: update meta tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -18,6 +18,18 @@ export default function Head() {
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English" />
       <meta name="author" content="Jiseeeh" />
+      <meta property="og:title" content="Recipe Realm" />
+      <meta property="og:site_name" content="Recipe Realm" />
+      <meta property="og:url" content="https://recipe-realm-j.vercel.app" />
+      <meta
+        property="og:description"
+        content="Share your legendary recipes with everyone!"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://cdn.britannica.com/97/234797-050-ADA29396/Okaka-salmon-onigiri.jpg"
+      />
       <link rel="icon" href="/food.ico" />
     </NextHead>
   );
